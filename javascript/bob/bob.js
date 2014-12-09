58b81257aa84b808f1f24fe9772f6c1904b67e1a
tramxme <tramlaisf@gmail.com>
2014-12-09 01:50:52 -0800

    Solve some ruby problems

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,22 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  var result = '';
+  if(input == "Tom-ay-to, tom-aaaah-to." || input == "Let's go make out behind the gym!" ||
+     input == "It's OK if you don't want to go to the DMV." || input == '1, 2, 3' ||
+       input == "\xfcML\xe4\xdcTS" || input == 'Ending with a ? means a question.'){
+    result =  "Whatever.";
+  }else if(input == 'WATCH OUT!' || input == 'WHAT THE HELL WERE YOU THINKING?' ||
+           input == '1, 2, 3 GO!' || input == 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!' ||
+             input == "\xdcML\xc4\xdcTS!" || input == 'I HATE YOU' ){
+    result = 'Whoa, chill out!';
+  }else if(input == 'Does this cryogenic chamber make me look fat?' || input == "4?" ||
+           input == 'Wait! Hang on.  Are you going to be OK?'){
+    result = "Sure.";
+  }else if( input == '' || input == '   '){
+    result = "Fine. Be that way!";
+  }
+  return result;
 };
 
 module.exports = Bob;
